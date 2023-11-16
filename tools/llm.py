31c3c3ab30161a58107dd31177e651be6e813546
zhangzy <zhangzy0816@gmail.com>
2023-11-16 16:01:11 +0800

    修改拼写错误

--- tools/llm.py
+++ tools/llm.py
@@ -8,11 +8,11 @@ def GetLLM(model: str, callback: BaseCallbackHandler = None):
 		os.environ["OPENAI_API_KEY"] = os.getenv('OPENAI_API_KEY')
 		os.environ["OPENAI_API_BASE"] = os.getenv('OPENAI_API_BASE')
 		return ChatOpenAI(temperature=0.9, model_name='gpt-3.5-turbo', streaming=True, callbacks=[callback], verbose=True)
-	elif model == 'huanyuan':
+	elif model == 'hunyuan':
 		return ChatHunyuan(
-			hunyuan_app_id=os.getenv('HUANYUAN_APP_ID'),
-    			hunyuan_secret_id=os.getenv('HUANYUAN_SECRET_ID'),
-   			hunyuan_secret_key=os.getenv('HUANYUAN_SECRET_KEY'),
+			hunyuan_app_id=os.getenv('HUNYUAN_APP_ID'),
+    			hunyuan_secret_id=os.getenv('HUNYUAN_SECRET_ID'),
+   			hunyuan_secret_key=os.getenv('HUNYUAN_SECRET_KEY'),
     			streaming=True,
 		)
 	elif model == 'chatgml':
